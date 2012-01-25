ad6b7b410700f1dfe1678d23f3fb6023390625e9
jonas <jonasbru@wanadoo.fr>
2012-01-25 10:04:42 +0100

    Actions et actionneurs

--- server/src/actions.h
+++ server/src/actions.h
@@ -2,8 +2,16 @@
 // ########################### Actionneurs ###########################
 
 
-
-struct ACTION{
-    char nom[20];
-
+struct actionFct_t{
+    void (*fct)(char[8]);
+    char id[8];
+    struct actionFct_t * nextActionFct;
 };
+
+struct action_t{
+    char nom[20];
+    struct actionFct_t * actionFcts;
+    struct action_t * nextAction;
+};
+
+struct action_t * actions;
