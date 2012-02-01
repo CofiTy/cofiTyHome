7d19cbb3b9ef7c1fc6aa39d2df32f3fc1bb5ae6b
Dorvaryn <benjamin@odai.homelinux.net>
2012-02-01 15:29:12 +0100

    Server Prompt epoll

--- server/src/main.c
+++ server/src/main.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <sys/epoll.h>
+#include <unistd.h> //Pour STDIN_FILENO
 
 #include "guiNetwork.h"
 #include "sensorsNetwork.h"
@@ -7,24 +9,67 @@
 
 #include "../../kernel/memory/memory.h"
 #include "actionneurs.h"
+#define MAX_EVENTS 10
 
 int main(int argc, char ** argv){
-	
-	initMemory();
+  struct epoll_event ev;
+  memset(&ev, 0, sizeof(struct epoll_event));
+  int epollfd;
+
+  epollfd = epoll_create(10);
+  FAIL(epollfd);
+
+  //Ajout à epoll de l'entrée standard
+  ev.events = EPOLLIN;
+  ev.data.fd = STDIN_FILENO;
+  FAIL(epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev));
+
+  struct epoll_event events[MAX_EVENTS];
+
+  int nfds;
+  int done = 0;
+
+  initMemory();
 
   parseAll();
 
 
-	printf("MemTotale: %ld\n", getGMemTotal());
-	printf("MemFree: %ld\n", getGMemFree());
+  printf("MemTotale: %ld\n", getGMemTotal());
+  printf("MemFree: %ld\n", getGMemFree());
 
-    sensorsNetworkStart();
-    guiNetworkStart();
-    sleep(120);
-    guiNetworkStop();
-    sensorsNetworkStop();
+  sensorsNetworkStart();
+  guiNetworkStart();
+  while(done == 0) //Boucle principale
+  {
 
+    nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+    FAIL(nfds);
 
+    int n;
+
+    for (n = 0; n < nfds; ++n)
+    {
+      if(events[n].data.fd == STDIN_FILENO)
+      {
+        char chaine[16];
+        fgets(chaine, 15, stdin); //récupère l'entrée standard
+
+        //Traite la commande
+        if(strcmp(chaine, "exit\n") == 0 || strcmp(chaine, "quit\n") == 0 || strcmp(chaine, "q\n") == 0)
+        {
+          done = 1;
+          puts("Quiting server!");
+        }
+        else
+        {
+          puts("Commande invalide\nUsage: exit, quit, q");
+        }
+        done = 1;
+      }
+    }
+  }
+  guiNetworkStop();
+  sensorsNetworkStop();
 
   return 0;
 }
