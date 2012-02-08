a44fd093fd1d999971d9152084022d785d7a916f
Dorvaryn <benjamin@odai.homelinux.net>
2012-02-08 16:38:09 +0100

    Fixed rules

--- server/src/sensors.c
+++ server/src/sensors.c
@@ -3,15 +3,12 @@
 
 void logValue(char idSensor[9], char nameValue[20], int value) {
     FILE * pFile;
-    puts("try Open");
     pFile = fopen("GHome.log", "a+");
-    puts("Open");
     time_t t;
     time(&t);
 
     if (pFile != NULL) {
         fprintf(pFile, "%d %s %s %d\n", (int)t, idSensor, nameValue, value);
-        puts("Write");
         fclose(pFile);
     } else {
         printf("Le fichier GHome.log n'a pas pu être ouvert !!!\n");
