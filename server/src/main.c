e173458a4dd24d8b447bdd635da6d2cd60c1bc6e
Dorvaryn <benjamin@odai.homelinux.net>
2012-02-15 09:14:41 +0100

    Add command line for reload files

--- server/src/main.c
+++ server/src/main.c
@@ -7,57 +7,62 @@
 #include "common.h"
 
 #include "init.h"
+#include "../parse/rules.tab.h"
 
 #define MAX_EVENTS 10
 
 int main(int argc, char ** argv) {
 
 
-    init();
+  init();
 
-    struct epoll_event ev;
-    memset(&ev, 0, sizeof (struct epoll_event));
-    int epollfd;
+  struct epoll_event ev;
+  memset(&ev, 0, sizeof (struct epoll_event));
+  int epollfd;
 
-    epollfd = epoll_create(10);
-    FAIL(epollfd);
+  epollfd = epoll_create(10);
+  FAIL(epollfd);
 
-    //Ajout à epoll de l'entrée standard
-    ev.events = EPOLLIN;
-    ev.data.fd = STDIN_FILENO;
-    FAIL(epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev));
+  //Ajout à epoll de l'entrée standard
+  ev.events = EPOLLIN;
+  ev.data.fd = STDIN_FILENO;
+  FAIL(epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev));
 
-    struct epoll_event events[MAX_EVENTS];
+  struct epoll_event events[MAX_EVENTS];
 
-    int nfds;
-    int done = 0;
+  int nfds;
+  int done = 0;
 
 
-    while (done == 0) //Boucle principale
-    {
+  while (done == 0) //Boucle principale
+  {
 
-        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-        FAIL(nfds);
+    nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+    FAIL(nfds);
 
-        int n;
+    int n;
 
-        for (n = 0; n < nfds; ++n) {
-            if (events[n].data.fd == STDIN_FILENO) {
-                char chaine[16];
-                fgets(chaine, 15, stdin); //récupère l'entrée standard
+    for (n = 0; n < nfds; ++n) {
+      if (events[n].data.fd == STDIN_FILENO) {
+        char chaine[16];
+        fgets(chaine, 15, stdin); //récupère l'entrée standard
 
-                //Traite la commande
-                if (strcmp(chaine, "exit\n") == 0 || strcmp(chaine, "quit\n") == 0 || strcmp(chaine, "q\n") == 0) {
-                    done = 1;
-                    puts("Quiting server!");
-                } else {
-                    puts("Commande invalide\nUsage: exit, quit, q");
-                }
-            }
+        //Traite la commande
+        if (strcmp(chaine, "exit\n") == 0 || strcmp(chaine, "quit\n") == 0 || strcmp(chaine, "q\n") == 0) {
+          done = 1;
+          puts("Quiting server!");
+        } else if (strcmp(chaine, "reload\n") == 0 ||  strcmp(chaine, "update\n") == 0 || strcmp(chaine, "u\n") == 0){
+          cleanMemory();
+          parseAll();
         }
+        else {
+          puts("Commande invalide\nUsage: exit, quit, q");
+        }
+      }
     }
-    destroy();
+  }
+  destroy();
 
-    return 0;
+  return 0;
 }
 
