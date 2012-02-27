63c65fcbfb09bdc0ba93f0f9029c815936d78784
crina <ccrina89@gmail.com>
2012-02-27 15:40:12 +0100

    comments sensors

--- server/src/sensors.c
+++ server/src/sensors.c
@@ -16,6 +16,8 @@ void logValue(char idSensor[SIZE_ID], char nameValue[SIZE_NAME], int value) {
     }
 }
 
+/*methode permetant de retrouver le capteur a partir de son id ou de son nom
+ la liste de capteurs existants est parcurue*/
 struct sensorType * getSensor(char id_or_name[MAX(SIZE_NAME,SIZE_ID)]) {
     struct sensorType * current = sensors;
 
@@ -30,36 +32,38 @@ struct sensorType * getSensor(char id_or_name[MAX(SIZE_NAME,SIZE_ID)]) {
     return 0;
 }
 
+/*methode permettant de decoder une trame*/
 void decodeTrame(char* trame) {
-    pthread_mutex_lock(&sensorsMutex);
+    pthread_mutex_lock(&sensorsMutex); //liste de capteurs protegee par mutex
 
     int i;
     char id[SIZE_ID];
-
+//recuperation de l'ID du capteur emmeteur
     for (i = 0; i < 8; i++) {
         id[i] = trame[i + 16];
     }
 
     id[8] = '\0';
-
+//recuperation de la structure de donnees correspondante 
     sensorType* capteur = getSensor(id);
-
+    //si le capteur est reconnu, appel a sa methode decode
     if (capteur != NULL) {
         capteur->decode(trame, capteur);
-        sem_post(&checkRules);
+        sem_post(&checkRules); //mise de jeton dans le sem pour le regles -> notification nouvelle trame
         
     }
-    pthread_mutex_unlock(&sensorsMutex);
+    pthread_mutex_unlock(&sensorsMutex);//liberation de la liste de capteurs
     
 }
 
+/*methode de decodage de donnees du capteur de presence et luminosite*/
 void decodePresence(char* trame, struct sensorType* capteur) {
     //TYPE: 06
     //FUNC: 01
     int presence;
     char and = '1';
 
-    if (((trame[15]>>1)&(and)) == and) //le bit 1 du octet 0 
+    if (((trame[15]>>1)&(and)) == and) //le bit 1 du octet 0 , 
         presence = 0;
     else
         presence = 1;
@@ -72,7 +76,7 @@ void decodePresence(char* trame, struct sensorType* capteur) {
 
     int valDec = strtol(data, &valHex, 16);
 
-    float lumin = 2*(float) valDec; // 2= ~510/256
+    float lumin = 2*(float) valDec; // 2= ~510/256, conforme a la doc
 
     ((dataPRESENCE*) capteur->data)->presence = presence;
     ((dataPRESENCE*) capteur->data)->luminosite = lumin;
@@ -82,8 +86,9 @@ void decodePresence(char* trame, struct sensorType* capteur) {
 
 }
 
+/*methode de decodage de donnees de l'interrupteur*/
 void decodeInterrupteur(char* trame, struct sensorType* capteur) {
-    char db3 = trame[8];
+    char db3 = trame[8]; //data bit 3
     switch (db3) {
         case '7':
         {
@@ -119,7 +124,7 @@ void decodeInterrupteur(char* trame, struct sensorType* capteur) {
     }
             logValue(capteur->id, "switchButton", ((dataINTERRUPTEUR*) capteur->data)->switchButton);
 }
-
+/*methode de decodage de donnees du capteur contact fenetre*/
 void decodeContact(char* trame, struct sensorType* capteur) {
     char and = '1';
     if (((trame[15])&(and)) == and) {
@@ -132,6 +137,7 @@ void decodeContact(char* trame, struct sensorType* capteur) {
 
 }
 
+/*methode de decodage de donnees du capteur de temperature*/
 void decodeTemperature(char* trame, struct sensorType* capteur) {
     char data[2];
     data[0] = trame[12];
