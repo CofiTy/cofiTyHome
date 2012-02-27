77625e39df209b384d3cb847f46e0f46a060108e
Dorvaryn <benjamin@odai.homelinux.net>
2012-02-27 23:11:30 +0100

    add timedCHeck files

---
+++ server/src/timedCheck.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <signal.h>
+
+#include "timedCheck.h"
+
+int sleepTime;
+
+void catchedAlarm(int sig_num) {
+      sem_post(&checkRules);
+      alarm(sleepTime);
+}
+
+void startTimedCheck(int nbSec){
+  signal(SIGALRM, catchedAlarm);
+  sleepTime = nbSec;
+  alarm(sleepTime);
+}
+
+void stopTimedCheck(){
+  signal(SIGALRM, NULL);
+}
---
+++ server/src/timedCheck.h
@@ -0,0 +1,10 @@
+#if ! defined ( TIM_H_ )
+#define TIM_H_
+
+#include "common.h"
+
+void startTimedCheck(int nbSec);
+
+void stopTimedCheck();
+
+#endif /*TIM*/
